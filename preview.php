6e8b36395e392a43a37c0e04a4671da823470973
rajarshibemech <rajarshibemech@gmail.com>
2017-02-20 17:29:21 +0800

    Add files via upload

---
+++ preview.php
@@ -0,0 +1,355 @@
+
+<!-- saved from url=(0158)file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ADD ELEMENT</title>
+    <!-- Bootstrap -->
+    <link href="./Add Element_1_files/bootstrap.min.css" rel="stylesheet">
+    <!-- for fontawesome icon css file -->
+    <link href="./Add Element_1_files/font-awesome.min.css" rel="stylesheet">
+    <!-- superslides css -->
+    <link rel="stylesheet" href="./Add Element_1_files/superslides.css">
+    <!-- for content animate css file -->
+    <link rel="stylesheet" href="./Add Element_1_files/animate.css">    
+    <!-- slick slider css file -->
+    <link href="./Add Element_1_files/slick.css" rel="stylesheet">        
+    <!-- website theme color file -->   
+     <link id="switcher" href="./Add Element_1_files/cyan-theme.css" rel="stylesheet">    
+    <!-- main site css file -->    
+    <link href="./Add Element_1_files/style.css" rel="stylesheet">    
+    <!-- google fonts  -->  
+    <link href="./Add Element_1_files/css" rel="stylesheet" type="text/css">    
+    <link href="./Add Element_1_files/css(1)" rel="stylesheet" type="text/css">  
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/img/favicon.ico" type="image/x-icon"> 
+	
+	<link href="./Add Element_1_files/treestyle.css" rel="stylesheet" type="text/css">
+	<link href="./Add Element_1_files/ChatStyle.css" rel="stylesheet" type="text/css">
+	
+	  
+	 
+<script type="text/javascript" charset="UTF-8" src="./Add Element_1_files/common.js.download"></script><script type="text/javascript" charset="UTF-8" src="./Add Element_1_files/util.js.download"></script><script type="text/javascript" charset="UTF-8" src="./Add Element_1_files/stats.js.download">
+
+</script>
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+</head>
+
+<body style="background-image: url(&quot;img/Scanbox_149.jpg&quot;); overflow: visible;" preloader"="">
+    <div id="status" style="transition-property: all; transition-duration: 0s; transition-timing-function: ease; display: none;">&nbsp;</div>
+  
+ 
+  <!-- End Preloader -->   
+  <a class="scrollToTop" href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#"><i class="fa fa-angle-up"></i></a>
+  
+  <!-- start navbar -->
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand">SMART <span>Structuring Manufacturing Architecture for Robotic Technologies</span></a>
+        <!-- <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="logo"></a> -->
+      </div>
+      <div id="navbar" class="navbar-collapse collapse navbar_area">          
+        <ul class="nav navbar-nav navbar-right custom_nav">
+          <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/HomePage.html">Home</a></li>
+		  <li>
+			<a style="color:#06d0d8" href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#" class="active" data-toggle="dropdown" role="button" aria-expanded="false">Knowledge Zone<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/KnowledgeManager.html">Knowledge Manager</a></li>            
+            </ul>
+          </li>
+          <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Training.html">Training</a></li>  
+		  <li class="active">
+            <a style ="color:#5e5e5e" href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Support<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#">FAQ</a></li>
+              <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#">Forums</a></li>
+              <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/ContactUs.html">Contact Us</a></li>               
+            </ul>
+          </li>
+		  <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/LoginRegister.html">Log in</a></li>		  
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
+  <br>
+
+<br><br><br><br>
+
+<style type="text/css">
+
+.form-style-10{
+  max-width:700px;
+  padding:30px;
+  margin:40px auto;
+  background: #FFF;
+  border-radius: 10px;
+  -webkit-border-radius:10px;
+  -moz-border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+  -moz-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+  -webkit-box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+}
+.form-style-10 .inner-wrap{
+  padding: 30px;
+  background: #F8F8F8;
+  border-radius: 6px;
+  margin-bottom: 15px;
+}
+.form-style-10 h1{
+  background: #2A88AD;
+  padding: 20px 30px 15px 30px;
+  margin: -30px -30px 30px -30px;
+  border-radius: 10px 10px 0 0;
+  -webkit-border-radius: 10px 10px 0 0;
+  -moz-border-radius: 10px 10px 0 0;
+  color: #fff;
+  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
+  font: normal 30px 'Bitter', serif;
+  -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+  -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+  box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+  border: 1px solid #257C9E;
+}
+.form-style-10 h1 > span{
+  display: block;
+  margin-top: 2px;
+  font: 13px Arial, Helvetica, sans-serif;
+}
+.form-style-10 label{
+  display: block;
+  font: 13px Arial, Helvetica, sans-serif;
+  color: #888;
+  margin-bottom: 15px;
+}
+.form-style-10 input[type="text"],
+.form-style-10 input[type="date"],
+.form-style-10 input[type="datetime"],
+.form-style-10 input[type="email"],
+.form-style-10 input[type="number"],
+.form-style-10 input[type="search"],
+.form-style-10 input[type="time"],
+.form-style-10 input[type="url"],
+.form-style-10 input[type="password"],
+.form-style-10 textarea,
+.form-style-10 select {
+  display: block;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  width: 100%;
+  padding: 8px;
+  border-radius: 6px;
+  -webkit-border-radius:6px;
+  -moz-border-radius:6px;
+  border: 2px solid #fff;
+  box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+  -moz-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+  -webkit-box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.33);
+}
+
+.form-style-10 .section{
+  font: normal 20px 'Bitter', serif;
+  color: #2A88AD;
+  margin-bottom: 5px;
+}
+.form-style-10 .section span {
+  background: #2A88AD;
+  padding: 5px 10px 5px 10px;
+  position: absolute;
+  border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border: 4px solid #fff;
+  font-size: 14px;
+  margin-left: -45px;
+  color: #fff;
+  margin-top: -3px;
+}
+.form-style-10 input[type="button"], 
+.form-style-10 input[type="submit"]{
+  background: #2A88AD;
+  padding: 8px 20px 8px 20px;
+  border-radius: 5px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  color: #fff;
+  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.12);
+  font: normal 30px 'Bitter', serif;
+  -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+  -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+  box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.17);
+  border: 1px solid #257C9E;
+  font-size: 15px;
+}
+.form-style-10 input[type="button"]:hover, 
+.form-style-10 input[type="submit"]:hover{
+  background: #2A6881;
+  -moz-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+  -webkit-box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+  box-shadow: inset 0px 2px 2px 0px rgba(255, 255, 255, 0.28);
+}
+.form-style-10 .privacy-policy{
+  float: right;
+  width: 250px;
+  font: 12px Arial, Helvetica, sans-serif;
+  color: #4D4D4D;
+  margin-top: 10px;
+  text-align: right;
+}
+
+.form-style-10.
+</style>
+</head>
+
+<body>
+
+
+<div class="form-style-10" >
+<h1>Preview<span>Preview your new knowledge point here</span></h1>
+<form action= "process_add_element.php" method="post" id="submitform">
+    <div class="section" id= "descriptor-name"><span>1</span>Descriptor Name</div>
+    <div class="inner-wrap" id = descriptor-name-inner>
+         <?php echo $_POST["descriptor-name"]; ?>
+    </div>
+    <div class="section" id= "status-inner"><span>2</span>Status</div>
+      <div class="inner-wrap"> New </div>
+
+     <div class="section " id = "knowledge-source"><span>3</span>Knowledge Source</div>
+                              
+       <div class="inner-wrap" id ="knowledge-source-inner"> 
+                                        
+                                 <?php echo $_POST["source-selector"]; ?>
+                                 <div> <span style = "font-weight: bold">Other Knowledge Sources      </span><?php echo $_POST["knowledge-source-input"]; ?></div>
+                                 </div>
+
+     <div class="section" id= "keywords"><span>4</span>Keywords</div>
+    <div class="inner-wrap" id= "keywords-inner">
+          <?php echo $_POST["keywords"]; ?>
+    </div>
+
+        
+        <div class= "section" id = "description"><span>5</span> Description</div>
+        <div class="inner-wrap" id = "description-inner">
+        <?php echo $_POST["description"]; ?>
+    </div>
+    
+
+    <div class="button-section">
+    <input type= "button" value="Images"/>
+    <input type= "button" value="Audio"/>
+    <input type= "button" value="Video"/>
+    <input type= "submit" value="Submit"/>
+    </div>
+</form>
+</div>
+
+  
+  <!-- start special quote -->
+ 
+  <!-- End special quote -->
+  
+
+    <!-- start footer -->
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+          <div class="footer_top">
+            <div class="row">
+              <div class="col-lg-4 col-md-4 col-sm-4">
+                <div class="single_footer_top">
+                  <h2>Why choose us? </h2>
+                  <ul>
+                    <li><a>We create significant impact</a></li>
+                    <li><a>Our products are scalable</a></li>
+                    <li><a>Our services are top notch</a></li>
+                    <li><a>We have branded customers</a></li>
+                    <li><a>Our features are unparalleled</a></li>
+                    <li><a>We are the first of our kind</a></li>
+                    <li><a>Our team is friendly and helpful</a></li>
+                    <li><a>We provide excellent support</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-4">
+                <div class="single_footer_top">
+                  <h2>SMART</h2>
+                  <p>SMART (Structuring Manufacturing Architecture for Robotic Technologies) is an integrated knowledge management system to assist operators as well as to coach/train novices to navigate the complex knowledge of robotic technologies.</p>
+                </div>
+                <div class="single_footer_top contact_mail">
+                  <h2>Contact us</h2>
+                  <p>We are always open for a quick chat! Give us a call or email us any time and we will respond shortly. <span>+1 888-522-0212</span> or <a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#">smart@onlysmart.com</a></p>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-4">
+                <div class="single_footer_top">
+                  <h2>Links </h2>
+                  <ul>
+                    <li><a  style ="color: #5e5e5e"href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/ContactUs.html">Support</a></li>
+                    <li><a>Blog</a></li>
+                    <li><a>FAQ</a></li>
+                    <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Testimonials.html">Testimonial Archive</a></li>
+                    <li><a>Careers</a></li>
+                    <li><a>Developer API</a></li>
+                    <li><a>Terms &amp; Conditions</a></li>
+                  </ul>
+                </div>
+                <div class="single_footer_top">
+                  <h2>Social Links </h2>
+                  <ul class="social_nav">
+                    <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#"><i class="fa fa-youtube"></i></a></li>
+                    <li><a href="file:///C:/Users/HP/Dropbox/KM-Expert%20System%20Project/SMART%20Project%20Documents/Prototype%20Implementation/Prototype_Implementation_v1/Add%20Element.html#"><i class="fa fa-linkedin"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>        
+        <div class="col-lg-12 col-md-12 col-sm-12">
+          <div class="footer_bottom">
+            <div class="copyright">
+              <p>All rights reserved </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End footer -->
+  
+
+
+<!-- jQuery Library -->
+<script src="./Add Element_1_files/jquery.min.js.download"></script>
+<!-- For content animatin  -->
+<script src="./Add Element_1_files/wow.min.js.download"></script>
+<!-- bootstrap js file -->
+<script src="./Add Element_1_files/bootstrap.min.js.download"></script> 
+<!-- superslides slider -->
+<script src="./Add Element_1_files/jquery.easing.1.3.js.download"></script>
+<script src="./Add Element_1_files/jquery.animate-enhanced.min.js.download"></script>
+<script src="./Add Element_1_files/jquery.superslides.min.js.download" type="text/javascript" charset="utf-8"></script>
+<!-- slick slider js file -->
+<script src="./Add Element_1_files/slick.min.js.download"></script>
+<!-- Google map -->
+<script src="./Add Element_1_files/js"></script>
+<script src="./Add Element_1_files/jquery.ui.map.js.download"></script>
+
+<!-- custom js file include -->
+<script src="./Add Element_1_files/custom.js.download"></script>  
+  
+</body></html>
